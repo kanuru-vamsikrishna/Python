42bb50333424adf1a7b105b8b17b6478f3f994f7
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-03-26 08:20:02 +0530

    Convert number to string

---
+++ num_to_string.py
@@ -0,0 +1,11 @@
+"""
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+"""
+
+def number_to_string(num):
+    return str(num)
+
+result = number_to_string(79585)
+print("Result:", result, type(result))
