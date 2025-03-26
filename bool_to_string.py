a1f1aca2c2540a86b05713dc6bfce165dd863f4f
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-03-26 08:24:35 +0530

    Converting boolean to string

---
+++ bool_to_string.py
@@ -0,0 +1,8 @@
+"""
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+"""
+def bool_to_word(boolean):
+    return "Yes" if boolean == True else "No"
+
+result = bool_to_word(True)
+print("Result:", result)
