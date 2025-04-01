6e45dae447c0f64a667fa90fece21922e7c5e006
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-04-01 15:09:39 +0530

    get final grades based on exam and project

---
+++ if_else_conditions.py
@@ -0,0 +1,22 @@
+"""
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+"""
+
+def final_grade(exam, projects):
+    if exam > 90 or projects > 10:
+        return 100
+    elif exam > 75 and projects >= 5:
+        return 90
+    elif exam > 50 and projects >= 2:
+        return 75
+    else:
+        return 0
+
+result1 = final_grade(100, 12)
+result2 = final_grade(85, 5)
+
+print("Result1:", result1)
+print("Result2:", result2)
